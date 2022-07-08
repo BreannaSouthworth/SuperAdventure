c2427dc65acb553c2a8335cff88b97da0c6382b6
Breanna Southworth <breannajane97@yahoo.com>
2022-07-08 16:56:58 -0400

    Add files via upload

---
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,15 @@
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        public SuperAdventure()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+    }
+}
